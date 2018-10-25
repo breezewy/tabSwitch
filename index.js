a35f2dd27401c6615669e54d74dfa0cd18b268cc
breezewy <15658150268@163.com>
2018-10-25 23:14:38 +0800

    second commit

--- index.js
+++ index.js
@@ -1,17 +1,5 @@
 
-// $('.mod-tab .tab').on('click',function(){
-//     $(this).addClass('active')
-//            .siblings()
-//            .removeClass('active')
-           
-//     $(this).parents('.mod-tab')
-//            .find('.panel')
-//            .eq($(this).index())
-//            .addClass('active')
-//            .siblings()
-//            .removeClass('active')    
-// })
-
+// jQuery 写法
 $('.mod-tab .tab').on('click',function(){
     $(this).addClass('active')
            .siblings()
@@ -26,3 +14,21 @@ $('.mod-tab .tab').on('click',function(){
 })
 
 
+// 原生JS写法
+// document.querySelectorAll('.mod-tab .tab').forEach(function(node){
+//     node.addEventListener('click',function(){
+//         var index
+//         this.parentElement.querySelectorAll('.tab').forEach(function(tab,idx){
+//             tab.classList.remove('active');
+//             if(node === tab){
+//                 index = idx;
+//             }
+//         })
+//         this.classList.add('active');
+//         this.parentElement.nextElementSibling.querySelectorAll('.panel').forEach(function(panel){
+//             panel.classList.remove('active');
+//         })
+//         this.parentElement.nextElementSibling.querySelectorAll('.panel')[index].classList.add('active');
+        
+//     })
+// })
