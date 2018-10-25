a747377983e6c774ac956bd8ebb5645b4b0225b6
breezewy <15658150268@163.com>
2018-10-25 19:42:56 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+jQuery写 tab切换
---
+++ index.js
@@ -0,0 +1,28 @@
+
+// $('.mod-tab .tab').on('click',function(){
+//     $(this).addClass('active')
+//            .siblings()
+//            .removeClass('active')
+           
+//     $(this).parents('.mod-tab')
+//            .find('.panel')
+//            .eq($(this).index())
+//            .addClass('active')
+//            .siblings()
+//            .removeClass('active')    
+// })
+
+$('.mod-tab .tab').on('click',function(){
+    $(this).addClass('active')
+           .siblings()
+           .removeClass('active')
+
+    $(this).parents('.mod-tab')
+           .find('.panel')
+           .eq($(this).index())
+           .addClass('active')
+           .siblings()
+           .removeClass('active')
+})
+
+
